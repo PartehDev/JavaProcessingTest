6dcc482490073574ca6947e66c7ef9466592bbf4
PartehDev <89151213+PartehDev@users.noreply.github.com>
2021-08-25 15:10:29 -0400

    Create ClickWindow.java

---
+++ src/alpha001/ClickWindow.java
@@ -0,0 +1,20 @@
+import processing.core.PApplet;
+
+public class Sketch extends PApplet {
+
+  public void settings() {
+    size(400, 400);
+  }
+
+  public void setup() {
+    background(0);
+  }
+
+  public void draw() {
+    
+  }
+  public void mouseClicked() {
+    fill(255);
+    ellipse(mouseX, mouseY, 25, 25);
+  }
+}
